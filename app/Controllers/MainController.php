291f9feef5b68283b16258c99a8e1b0bf13fa9a1
ArianeSpa <ariane.spanneut@gmail.com>
2019-10-13 19:05:37 +0200

    GoalModel

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace oFeel\Controllers;
 
 use oFeel\Models\FoodModel;
 use oFeel\Models\DietModel;
+use oFeel\Models\GoalModel;
 
 
 // Classe qui ne sera jamais instanciée, uniquement héritée (abstract)
@@ -20,6 +21,11 @@ class MainController extends CoreController
         // $newDietModel = new DietModel;
         // $myDietList = $newDietModel->findDiet();
         // dump($myDietList);
+
+        // essai de récup des goal datas ok
+        $newGoalModel = new GoalModel;
+        $myGoalList = $newGoalModel->findGoal();
+        dump($myGoalList);
         
         $this->show('home');
     }
